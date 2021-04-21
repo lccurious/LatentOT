84bc9cf32fe3b0ca033704964eeacd35c1f53b8b
lccurious <lccurious@outlook.com>
2021-04-21 21:56:35 +0800

    Compare with original implementation

--- demos/simple_lot_test.py
+++ demos/simple_lot_test.py
@@ -1,30 +1,24 @@
-import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
-from scipy.stats import wishart, chi2
 
-import sys
-sys.path.append('../lot')
-
-from utils import generate_gmm_data, kmeans
-from latentot import LatentOT
+from lot import LOT
 
 n_components, n_dim = 4, 2
 # X = generate_gmm_data(n_components, n_dimensions=2).float()
 # Y = generate_gmm_data(n_components, n_dimensions=2).float()
-source_cores = np.asarray([[1, 1], [2, -1], [-3, -1], [-4, 1]]) * 8
+source_cores = np.asarray([[1, 1], [2, -1], [-3, -1], [-4, 1]]) * 4
 target_cores = source_cores / 2
 X, _ = make_blobs(n_samples=400, centers=source_cores)
+center_x = X.mean(axis=0)
 Y, _ = make_blobs(n_samples=400, centers=target_cores)
-X = torch.from_numpy(X).float()
-Y = torch.from_numpy(Y).float()
+center_y = Y.mean(axis=0)
 
-eps = 10
-lot = LatentOT(X, Y, 4, 4, eps, eps, eps, device=torch.device('cuda'))
-px, py, pz, zx, zy = lot.fit(max_iter=1000)
-px, py, pz, zx, zy = px.cpu(), py.cpu(), pz.cpu(), zx.cpu(), zy.cpu()
-X, Y = X.cpu(), Y.cpu()
+eps = 4
+lot_solver = LOT(n_cluster_source=4, n_cluster_target=4,
+                 epsilon=eps, epsilon_z=eps, intensity=[1, 1, 1], floyditer=50)
+
+px, py, pz, p, pot, zx, zy, t, converrlist = lot_solver(X, Y)
 
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 ax.scatter(X[:, 0], X[:, 1], c='b', zorder=10)
@@ -32,7 +26,14 @@ ax.scatter(Y[:, 0], Y[:, 1], c='r', zorder=10)
 ax.scatter(zx[:, 0], zx[:, 1], c='y', marker='^', zorder=20)
 ax.scatter(zy[:, 0], zy[:, 1], c='g', marker='^', zorder=20)
 ax.axis('equal')
+
 # plot line connect
+ax.scatter(center_x[0], center_x[1], marker='*', s=200)
+ax.scatter(center_y[0], center_y[1], marker='*', s=200)
+for i, (pt_x, pt_y) in enumerate(zip(zx, zy)):
+    ax.plot([center_x[0], pt_x[0]], [center_x[1], pt_x[1]], c='k')
+    ax.plot([center_y[0], pt_y[0]], [center_y[1], pt_y[1]], c='k')
+
 for i, p in enumerate(px):
     zi = p.argmax()
     ax.plot([X[i, 0], zx[zi, 0]], [X[i, 1], zx[zi, 1]], c='gray', alpha=0.5)
